6c3e56dc563322bc12023deebb1f9bb2f9fdfb86
Haifeng Liu <bladehliu@users.noreply.github.com>
2024-05-20 18:24:38 +0800

    Update doc.go

--- backend/doc.go
+++ backend/doc.go
@@ -2,11 +2,13 @@
 
 the backend server - 
 * maintains the chat history for each user as long-term memory
+* judges to search or not
 * rewrites the conversational query
 * retrieves from Web search APIs and/or self-hosted internal indexes (Vearch by default)
 * reranks the retrieval result
-* generats the answer by calling LLMs
+* generates the answer by calling LLMs
 * suggests related questions
+* can be planning and multi-step reasoning
 
 */
 
